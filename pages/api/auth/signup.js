70d2781dde4e4d41179677419ccd63ba8d79bb82
Karol Galera <frontend.galera@gmail.com>
2023-03-23 16:40:39 +0100

    api route signup with database connection, added to pages/api

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,34 @@
+import { hashPassword } from "../../../helpers/authUtil";
+import { connectToDatabase } from "../../../helpers/dbUtil";
+
+const handler = async (req, res) => {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    !password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 characters long",
+    });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const db = client.db();
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "Created user" });
+};
+
+export default handler;
