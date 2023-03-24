0aa2feb6b9a15874baf5248bed2b21f9a1b5f3d5
Karol Galera <frontend.galera@gmail.com>
2023-03-24 20:04:04 +0100

    authFormUtil file added to helpers

---
+++ helpers/authFormUtil.js
@@ -0,0 +1,17 @@
+export const createUser = async (email, password) => {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Somethingwent wrong");
+  }
+
+  return data;
+};
