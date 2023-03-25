281dff81f43c8315181aaa9afd65300b41fc36eb
Karol Galera <frontend.galera@gmail.com>
2023-03-25 19:27:38 +0100

    useSession hook added to MainNavigation component

--- components/Layout/MainNavigation/index.js
+++ components/Layout/MainNavigation/index.js
@@ -1,7 +1,10 @@
+import { useSession } from "next-auth/client";
 import Link from "next/link";
 import classes from "./styles.module.css";
 
 const MainNavigation = () => {
+  const [session, loading] = useSession();
+
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -9,15 +12,21 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
