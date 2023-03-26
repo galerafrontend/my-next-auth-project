61523d2d32a32223560f2486ab9135d5e3b225b1
Karol Galera <frontend.galera@gmail.com>
2023-03-26 22:18:12 +0200

    getServerSideProps with "/auth" route protection with getSession, added to AuthPage route component

--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,25 @@
+import { getSession } from "next-auth/client";
 import AuthForm from "../components/Auth";
 
 const AuthPage = () => {
   return <AuthForm />;
+};
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        pernament: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default AuthPage;
