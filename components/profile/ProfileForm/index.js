c95c3a19a54a61d820336554a446e3f95e1a931d
Karol Galera <frontend.galera@gmail.com>
2023-03-27 18:37:09 +0200

    onSubmitHandler function with onChangePassword props added to ProfileForm component

--- components/profile/ProfileForm/index.js
+++ components/profile/ProfileForm/index.js
@@ -1,15 +1,31 @@
 import classes from "./styles.module.css";
+import { useRef } from "react";
 
-const ProfileForm = () => {
+const ProfileForm = ({ onChangePassword }) => {
+  const oldPasswordInputRef = useRef();
+  const newPasswordInputRef = useRef();
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredOldPassword = oldPasswordInputRef.current.value;
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" />
+        <input type="password" id="new-password" ref={newPasswordInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="old-password">Old Password</label>
-        <input type="password" id="old-password" />
+        <input type="password" id="old-password" ref={oldPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
